94352359a1feefd56e074b9e1f920798cc8f0d02
Brady <brady.y.liu@gmail.com>
2019-06-20 16:14:24 -0700

    Setting up boiler plate for proxy

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+
+const port = '3010';
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname + '/index.html')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`App hosted on port ${port}`);
+});
